c155e43e80d6eaca82532c54ce39572cbd8e111f
Pratyansh Maddhesia <pratyansh35@gmail.com>
2023-09-26 13:11:10 +0530

    "dropDownMenu"

--- app/src/main/java/com/example/help_disaster/AfterForm.kt
+++ app/src/main/java/com/example/help_disaster/AfterForm.kt
@@ -2,10 +2,21 @@ package com.example.help_disaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class AfterForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after_form)
+        // get reference to the string array that we just created
+        val languages = resources.getStringArray(R.array.disaster_types)
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, languages)
+        // get reference to the autocomplete text view
+        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        // set adapter to the autocomplete tv to the arrayAdapter
+        autocompleteTV.setAdapter(arrayAdapter)
     }
 }
